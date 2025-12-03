1f8e796fb93f0c25e89aa02c4bffcaafdbaa6f6e
mausham-1224 <stha.mausham18@gmail.com>
2025-12-03 15:07:09 +0545

    all-content

---
+++ basic-input.py
@@ -0,0 +1,5 @@
+from functioncall import calculate_sum,fullname
+
+a=1
+b=2
+print(f"sum of two number is: {calculate_sum(a,b)}")
---
+++ functioncall.py
@@ -0,0 +1,4 @@
+def calculate_sum(a:int,b:int)-> int:
+    return a+b
+def fullname(first,last):
+    return f"hi {first} {last}"
