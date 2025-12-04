4c1a777d96e46c41f8bec4cf67e74d08aa5c9387
mausham-1224 <stha.mausham18@gmail.com>
2025-12-04 20:45:12 +0545

    math operations

--- basic-input.py
+++ basic-input.py
@@ -1,6 +1,21 @@
 from functioncall import calculate_sum,fullname
+import  math
+age=int(input("Enter your age:"))
+if age>=18:
+    print("you are elgible")
+else:
+    print("not elogible")
 
-a=1
-b=2
-print(f"sum of two number is chechhhh: {calculate_sum(a,b)}")
-print(fullname('mausham','shrestha'))
+operator=input("enter your operation (+,_,*,/):")
+num1=float(input("enter the number 1:"))
+num2=float(input("enter the second number"))
+if operator=="+":
+    print(f"sum of number is: {num1+num2}")
+elif operator=="-":
+    print(f"sum of number is: {num1-num2}")
+elif operator=="*":
+    print(f"sum of number is: {num1*num2}")
+elif operator=="/":
+    print(f"the did:{round( num1/num2)}")
+else:
+    print(f"operator is not valid :{operator}")
