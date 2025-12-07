68ce97e8ee411530b47d7e2e139b0198cd974301
mausham-1224 <stha.mausham18@gmail.com>
2025-12-07 21:59:27 +0545

    loop

---
+++ nestedloop.py
@@ -0,0 +1,9 @@
+
+rows= int(input("Enter the number of Rows:"))
+column= int(input("Enter the number of cows:"))
+symbol= input("Enter the character:")
+
+for y in range(rows):
+    print()
+    for x in range(column):
+        print(symbol, end="")
